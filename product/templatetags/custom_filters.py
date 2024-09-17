1bebe873d2eb32964da11eb50bc9da9bc7b0d094
tina <tina.amini.b@gmail.com>
2024-09-17 11:34:37 +0330

    replace english digits whit persian digits

--- product/templatetags/custom_filters.py
+++ product/templatetags/custom_filters.py
@@ -7,6 +7,13 @@ def iran_currency(value):
     try:
         value = int(value)
         # Format the number with Persian commas
-        return "{:,.0f}".format(value).replace(',', '٬') 
+        formatted_value = "{:,.0f}".format(value).replace(',', '٬')
+
+        # Replace English digits with Persian digits
+        persian_digits = {'0': '۰', '1': '۱', '2': '۲', '3': '۳', '4': '۴', '5': '۵', '6': '۶', '7': '۷', '8': '۸', '9': '۹'}
+        for en_digit, fa_digit in persian_digits.items():
+            formatted_value = formatted_value.replace(en_digit, fa_digit)
+
+        return formatted_value
     except (ValueError, TypeError):
         return value
