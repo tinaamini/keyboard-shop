a811a80b0aff8728ad32b1972e7d862a47231632
tina <tina.amini.b@gmail.com>
2024-11-20 18:01:43 +0330

    custom user

---
+++ account/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'full_name')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('پسورد ها یکسان نیستند')
+
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        help_text='شما میتوانید تغییر بدهید پسوردتونو با این <a/>فرم< href=\"../password/\" a >')
+
+    class Meta:
+        model = User
+
+        fields = ('email','phone_number', 'full_name', 'password', 'last_login')
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home',
     'product',
     "orders",
+    "account",
 
 
 ]
@@ -133,3 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL='account.User'
