d8a3b9fe8b6e04018c944b088c49a70931fe8401
tina <tina.amini.b@gmail.com>
2024-11-21 17:23:39 +0330

    UserRegisterVerify

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import UserCreationForm, UserChangeForm
-from .models import User
+from .models import User,OtpCode
 from django.contrib.auth.models import Group
 
 # Register your models here.
@@ -32,3 +32,6 @@ admin.site.register(User,UserAdmin)
 
 
 
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number','code','created')
--- utils.py
+++ utils.py
@@ -2,3 +2,10 @@
 def format_price(value):
     return "{:,}".format(value)
 
+
+import requests
+
+
+def send_otp_code(phone_number, code):
+    data = {'from': '50002710017796', 'to': phone_number, 'text': f'{code}'}
+    response = requests.post('https://console.melipayamak.com/api/receive/balance/e97f2fd3453d4717bd528e07a8210f0d', json=data)
